95bc3363f7e672ff1c383d7821745e40fc9e4419
Zeke <jkezekiel@gmail.com>
2021-08-28 14:20:27 -0500

    trying to become vue 3 compatible plugin

--- src/index.js
+++ src/index.js
@@ -2,18 +2,4 @@
 // import component from './my-component.vue';
 import SleekTest from './components/SleekTest/SleekTest.vue'
 
-export {
-  SleekTest
-}
-
-export function install(Vue, options) {
-  options = {
-    installComponents: true,
-    ...options
-  }
-
-  if(options.installComponents) {
-    Vue.component('SleekTest', SleekTest)
-  }
-}
-
+export default SleekTest;
